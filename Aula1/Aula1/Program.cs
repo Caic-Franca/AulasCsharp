b22f884a77d70fddb98ff500d13f255f1b23df7b
caic.lfmaria <caic.lfmaria@TIT0533380W10-1>
2025-02-25 19:27:18 -0300

    Adicionar arquivos de projeto.

---
+++ Aula1/Aula1/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Hello, World!");
+
+string firstname = "Caic";
+string surname = "França";
+string lastName = "Maria";
+
+Console.WriteLine($"Meu nome completo é {firstname} {surname} {lastName}");
+
+int idade = 30;
+
+Console.WriteLine($"E minha idade é {idade}");
+
+Console.WriteLine($"Escreva seu nome: ");
+
+string nome = Console.ReadLine();
+
+Console.WriteLine($"Olá {nome}, boa noite!");
